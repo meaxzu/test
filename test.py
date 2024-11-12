e74fbf9f6226201eca23bfa3e5d27dc5fb38d821
meaxzu <minju050224@cau.ac.kr>
2024-11-12 14:34:25 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
+for i in renge(5):
+  print("Hello world")
